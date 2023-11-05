2f021203ff7a76a32014a1f7c8a2ef0c674a9ef8
Daniel Owens <danielowenscs@gmail.com>
2023-11-04 22:03:46 -0400

    Initial Commit

---
+++ SpellingBee/SpellingBeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpellingBeeApp.swift
+//  SpellingBee
+//
+//  Created by Daniel Owens on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SpellingBeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
